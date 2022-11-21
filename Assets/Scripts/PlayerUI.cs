4b65fb69fcfe321c8209f66f361ce56eb8515317
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2022-11-21 16:05:28 +0100

    BarPv

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -8,7 +8,11 @@ using System;
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private Image _LifeBar;
+    private void Update()
+    {
+        float fillAmount = PlayerController.PlayerControllerInstance.CurrentLife / PlayerController.PlayerControllerInstance.Life;
+        _LifeBar.fillAmount = fillAmount;
+    }
 
 
-    
 }
