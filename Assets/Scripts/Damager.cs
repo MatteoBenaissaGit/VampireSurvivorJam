4bb57773c2f2e3102ab1ae9f1ca582cec23e9777
P3D\mbenaissa <matteobenaissa@gmail.com>
2022-11-21 14:49:40 +0100

    script class damager for bullets etc

---
+++ Assets/Scripts/Damager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+public abstract class Damager : MonoBehaviour
+{
+    [Header("Damager")] [SerializeField] protected float _damage;
+    [SerializeField] protected float _speed;
+
+    private Vector2 _direction;
+    protected Rigidbody2D _rigidbody2D;
+    public Damageable ParentDamageable;
+
+    public virtual void Set(Vector2 direction, Damageable damageable)
+    {
+        _direction = direction;
+        ParentDamageable = damageable;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    protected void Start()
+    {
+    }
+
+    protected void Update()
+    {
+        Move();
+    }
+
+    protected virtual void Move()
+    {
+        _rigidbody2D.velocity = _direction * _speed;
+    }
+
+    protected virtual void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Damageable damageable = collision.collider.gameObject.GetComponent<Damageable>();
+        if (damageable == null || damageable != ParentDamageable)
+        {
+            return;
+        }
+        
+        damageable.TakeDamage(_damage);
+        
+        DestroyObject();
+    }
+}
