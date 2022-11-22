dc3e6076ef5faa591b715f828f46b13c9a393d68
MatteoBenaissa <matteobenaissa@gmail.com>
2022-11-22 04:00:16 +0100

    speaker and shockwave

---
+++ Assets/Scripts/Damager/ShockWave.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class ShockWave : Damager
+{
+    [SerializeField] private float _attackDuration = 1.5f;
+    
+    private void Start()
+    {
+        base.Start();
+        transform.position = ParentDamageable.transform.position;
+        transform.DOScale(10, _attackDuration);
+        StartCoroutine(EndShockWave());
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+
+        _rigidbody2D.velocity = ParentDamageable.GetComponent<Rigidbody2D>().velocity;
+    }
+
+    protected override void DamageEnemy(Damageable damageable)
+    {
+        base.DamageEnemy(damageable);
+        
+        //enemy
+        EnemyController enemy = damageable.GetComponent<EnemyController>();
+        if (enemy != null)
+        {
+            enemy.GetPushed();
+        }
+    }
+
+    private IEnumerator EndShockWave()
+    {
+        yield return new WaitForSeconds(_attackDuration);
+        GetComponent<SpriteRenderer>().DOFade(0, 0.5f).OnComplete(DestroyObject);
+    }
+}
